837b79e614e51a311812eb1b20f6559e7beb978f
Charles Parker <charles.parker@mdpanel.com>
2024-06-09 21:04:26 -0700

    A first stab at the basic screens for the game

---
+++ webui/apps/website/app/scenario/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { ScenarioState } from "@/components/scenario-state";
+
+export default function ScenarioId({ params }: { params: { id: string } }) {
+  return <ScenarioState />;
+}
---
+++ webui/apps/website/app/scenario/new/page.tsx
@@ -0,0 +1,5 @@
+import { NewScenario } from "@/components/new-scenario";
+
+export default function ScenarioNew() {
+  return <NewScenario />;
+}
